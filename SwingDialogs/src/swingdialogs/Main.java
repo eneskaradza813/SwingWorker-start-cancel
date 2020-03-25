8e42cb68ed4a5dd2b4a2eb7cb904cba8712be815
mesa <mesa@DESKTOP-6T2EFT8>
2020-03-25 21:38:48 +0100

    22233444 java Javachoser filter
    = vjezba

--- SwingDialogs/src/swingdialogs/Main.java
+++ SwingDialogs/src/swingdialogs/Main.java
@@ -2,28 +2,59 @@ package swingdialogs;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.File;
 import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 
 public class Main {
 
     public static void main(String[] args) {
 
-      JFrame frame = new JFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(new Dimension(600, 400));
-      frame.setLocationRelativeTo(null);
-      
-      
-       Color backroundColor =  JColorChooser.showDialog(null, "Chose color", Color.yellow);
+        JFileChooser fileChoser = new JFileChooser();
         
-       if(backroundColor != null)
+        
+        fileChoser.setFileFilter(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                if(f.isDirectory())
+                {
+                    return true;
+                }
+                String fn = f.getName();
+                String e = "";
+                
+                int i = fn.lastIndexOf(".");
+                if(i>0 && i < fn.length() -1)
+                {
+                    e = fn.substring(i + 1).toLowerCase();
+                    
+                }
+                return e.equals("png");
+                }
+
+            @Override
+            public String getDescription() {
+                return "png images";
+            }
+        });
+        
+        
+        
+        
+        int response = fileChoser.showOpenDialog(null);
+        
+       switch(response)
        {
-           frame.getContentPane().setBackground(backroundColor);
+           case JFileChooser.CANCEL_OPTION:
+               break;
+           default:
+               JOptionPane.showMessageDialog(null, fileChoser.getSelectedFile().getPath());
+               break;
+               
        }
-       frame.setVisible(true);
-       
     }
 
 }
