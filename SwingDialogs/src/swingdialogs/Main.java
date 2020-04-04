e7b9b05d77b93d08395ce52926d7fd0cd0534ea3
mesa <mesa@DESKTOP-6T2EFT8>
2020-04-04 11:44:26 +0200

    2223344445555 java Gui JoptionPane.showOptionDialog
    = vjezba

--- SwingDialogs/src/swingdialogs/Main.java
+++ SwingDialogs/src/swingdialogs/Main.java
@@ -1,60 +1,25 @@
 package swingdialogs;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.io.File;
-import javax.swing.JColorChooser;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileFilter;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 
-        JFileChooser fileChoser = new JFileChooser();
+        int yourSide = JOptionPane.showOptionDialog(null, "Choose your ride", "option dialog", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[] {"Millenium falcon", "X-Wing","B-Wing", "y-Wing", "Imperial Battlecruiser"}, null);
+        if(yourSide == -1)
         
-        
-        fileChoser.setFileFilter(new FileFilter() {
-            @Override
-            public boolean accept(File f) {
-                if(f.isDirectory())
-                {
-                    return true;
-                }
-                String fn = f.getName();
-                String e = "";
-                
-                int i = fn.lastIndexOf(".");
-                if(i>0 && i < fn.length() -1)
-                {
-                    e = fn.substring(i + 1).toLowerCase();
-                    
-                }
-                return e.equals("png");
-                }
-
-            @Override
-            public String getDescription() {
-                return "png images";
-            }
-        });
+            JOptionPane.showMessageDialog(null, "You haven't choosen any vehicle");
+            else if(yourSide < 5)
+                JOptionPane.showMessageDialog(null, "You have choosen bright side vehicle");
+            else
+                JOptionPane.showMessageDialog(null, "You have choosen dark side vehicle");
+            
         
         
         
         
-        int response = fileChoser.showOpenDialog(null);
-        
-       switch(response)
-       {
-           case JFileChooser.CANCEL_OPTION:
-               break;
-           default:
-               JOptionPane.showMessageDialog(null, fileChoser.getSelectedFile().getPath());
-               break;
-               
-       }
     }
 
 }
