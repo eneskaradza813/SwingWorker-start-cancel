dd1abdf660967abcf5746c15663ea8cdd378c6b4
mesa <mesa@DESKTOP-6T2EFT8>
2020-04-06 21:54:16 +0200

    3211222 java Gui Swingworker start cancel
    = vjezba

--- Niti/src/niti/Main.java
+++ Niti/src/niti/Main.java
@@ -1,5 +1,6 @@
 package niti;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
@@ -13,58 +14,72 @@ import javax.swing.SwingWorker;
 
 public class Main {
 
+    static SwingWorker sw;
     public static void main(String[] args) throws InterruptedException, InvocationTargetException {
 
-        Runnable myGUI = new Runnable() {
-            @Override
-            public void run() {
-                JFrame f = new JFrame();
-                JButton button = new JButton("click");
-                button.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        SwingWorker sw = new SwingWorker<String, String>() {
-                            @Override
-                            protected void done() {
-                                try{
-                                    System.out.println(get());
+       Runnable myGUI = new Runnable() {
+           @Override
+           public void run() {
+               JFrame f = new JFrame();
+               f.setLayout(new FlowLayout());
+               
+               JButton button = new JButton("Start");
+               JButton btnC = new JButton("Cancel");
+               button.addActionListener(new ActionListener() {
+                   @Override
+                   public void actionPerformed(ActionEvent e) {
+                       sw = new SwingWorker<String, String>() {
+                           @Override
+                           protected void done() {
+                               try{
+                                   if(!isCancelled())
+                                       System.out.println(get());
+                                   else
+                                       System.out.println("canceled");
+                               }
+                               catch(InterruptedException exc){}
+                               catch(ExecutionException exc){}
                                 }
-                                catch(InterruptedException ex){}
-                                catch(ExecutionException ex){}
+                           
+                           @Override
+                           protected String doInBackground() throws Exception {
+                               int in = 0;
+                               while(!isCancelled())
+                               {
+                                   Thread.sleep(1000);
+                                   int i = 0;
+                                   publish("not done yet, i am on" + i++);
+                               }
+                               return "I am done";
                                 }
-                            
-                            @Override
-                            protected String doInBackground() throws Exception {
-                                int i = 0;
-                                while(true)
-                                {
-                                    Thread.sleep(1000);
-                                    publish("not done yet, i am on" + i++);
-                                }
-                                 }
 
-                            @Override
-                            protected void process(List<String> chunks) {
-                                System.out.println(chunks.get(0));
-                                 }
-                            
-                        };
-                        sw.execute();
-                         }
-                });
-                        
-                        
-                        
-                        
-                        
-                f.add(button);
-                f.setSize(300, 300);
-                f.setVisible(true);
-                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                 }
-        };
+                           @Override
+                           protected void process(List<String> chunks) {
+                               System.out.println(chunks.get(0));
+                                }
+                           
+                       };
+                       sw.execute();
+                        }
+               });
+               btnC.addActionListener(new ActionListener() {
+                   @Override
+                   public void actionPerformed(ActionEvent e) {
+                       sw.cancel(true);
+                        }
+               });
+               
+               
+               f.add(button);
+               f.add(btnC);
+               f.setSize(300, 300);
+               f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+               f.setVisible(true);
+               
+                }
+       };
+                
         SwingUtilities.invokeAndWait(myGUI);
-        
     }
 
 }
